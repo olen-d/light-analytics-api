b31c7ae80dd40a372a9e67b96adb6906633fdf3e
olen-d <olen.daelhousen@outlook.com>
2023-10-19 23:33:21 -0400

    Return bounce rate

--- routes/api/v1/sessions/index.js
+++ routes/api/v1/sessions/index.js
@@ -1,8 +1,14 @@
 'use strict'
-import { addSession, readSinglePageSessionsCountTotal, readVisitsCountTotal, readVisitsCountUnique } from '../../../../controllers/v1/session-controllers.mjs'
+import {
+  addSession,
+  readBounceRateTotal,
+  readSinglePageSessionsCountTotal,
+  readVisitsCountTotal, readVisitsCountUnique
+} from '../../../../controllers/v1/session-controllers.mjs'
 
 export default async function (fastify, opts) {
   fastify.get('/', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readVisitsCountTotal)
+  fastify.get('/bounce-rate', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readBounceRateTotal)
   fastify.get('/single-page', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readSinglePageSessionsCountTotal)
   fastify.get('/unique', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readVisitsCountUnique)
   fastify.post('/', { onRequest: fastify.auth([fastify.verifyAPIKey])}, addSession)
