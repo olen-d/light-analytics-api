517c4e65015000a482ca1ab010603b3971c6eb83
Olen Daelhousen <olen@roman.search.charter.com>
2024-03-10 23:07:03 -0400

    End point to return page views and total time

--- controllers/v1/page-controllers.mjs
+++ controllers/v1/page-controllers.mjs
@@ -1,7 +1,13 @@
 'use strict'
 
 import { sanitizeAll, trimAll } from '../../services/v1/input.mjs'
-import { getRoutesByTotalTime, getRoutesByTotalTimeViews, getRoutesByTotalViews, newPage } from '../../models/v1/page-models.mjs'
+import {
+  getPagesByTotalTimeViews,
+  getRoutesByTotalTime,
+  getRoutesByTotalTimeViews,
+  getRoutesByTotalViews,
+  newPage
+} from '../../models/v1/page-models.mjs'
 
 async function addPage (req, reply) {
   try {
@@ -31,6 +37,18 @@ async function addPage (req, reply) {
     reply.send(result)
   } catch (error) {
     throw new Error(`Page Controllers Add Page ${error}`)
+  }
+}
+
+async function readPagesByTotalTimeViews (request, reply) {
+  try {
+    const { _db } = this
+    const info = 'all'
+
+    const result = await getPagesByTotalTimeViews(_db, info)
+    reply.send(result)
+  } catch (error) {
+    throw new Error(`Page Controllers Read Pages by Total Views ${error}`)
   }
 }
 
@@ -70,4 +88,10 @@ async function readRoutesByTotalViews (req, reply) {
   }
 }
 
-export { addPage, readRoutesByTotalTime, readRoutesByTotalTimeViews, readRoutesByTotalViews }
+export {
+  addPage,
+  readPagesByTotalTimeViews,
+  readRoutesByTotalTime,
+  readRoutesByTotalTimeViews,
+  readRoutesByTotalViews
+}
--- routes/api/v1/pages/index.js
+++ routes/api/v1/pages/index.js
@@ -1,10 +1,14 @@
 'use strict'
-import { addPage, readRoutesByTotalTime, readRoutesByTotalTimeViews, readRoutesByTotalViews } from '../../../../controllers/v1/page-controllers.mjs'
+import {
+  addPage,
+  readPagesByTotalTimeViews,
+  readRoutesByTotalTime,
+  readRoutesByTotalTimeViews,
+  readRoutesByTotalViews
+} from '../../../../controllers/v1/page-controllers.mjs'
 
 export default async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return 'pages'
-  })
+  fastify.get('/', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readPagesByTotalTimeViews)
   fastify.get('/routes/total-time', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readRoutesByTotalTime)
   fastify.get('/routes/total-time-views', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readRoutesByTotalTimeViews)
   fastify.get('/routes/total-views', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readRoutesByTotalViews)
