521529a30d54cd562f58b18d7cdab5f73efafc75
Olen Daelhousen <olen@roman.search.charter.com>
2024-03-11 23:15:44 -0400

    Fix missing by day end point for page views

--- routes/api/v1/pages/index.js
+++ routes/api/v1/pages/index.js
@@ -1,14 +1,16 @@
 'use strict'
 import {
   addPage,
-  readPagesByTotalTimeViews,
+  readViewsCountTimeByDay,
+  readViewsCountTimeTotal,
   readRoutesByTotalTime,
   readRoutesByTotalTimeViews,
   readRoutesByTotalViews
 } from '../../../../controllers/v1/page-controllers.mjs'
 
 export default async function (fastify, opts) {
-  fastify.get('/', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readPagesByTotalTimeViews)
+  fastify.get('/', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readViewsCountTimeTotal)
+  fastify.get('/by-day', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readViewsCountTimeByDay)
   fastify.get('/routes/total-time', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readRoutesByTotalTime)
   fastify.get('/routes/total-time-views', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readRoutesByTotalTimeViews)
   fastify.get('/routes/total-views', { onRequest: fastify.auth([fastify.verifyAPIKey])}, readRoutesByTotalViews)
